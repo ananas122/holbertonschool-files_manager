823bce2b0f700c3c1589477da9d27a4366c4de81
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-04-02 13:34:32 +0200

    Create a new user

--- utils/db.js
+++ utils/db.js
@@ -7,23 +7,30 @@ class DBClient {
     const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}`;
 
+    // Connexion à la base de données MongoDB
     mongodb.MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      // Assignation de la base de données à l'instance de DBClient
       this.db = client.db(database);
     });
   }
 
+  // Méthode pour vérifier si la connexion à la base de données est active
   isAlive() {
     return !!this.db;
   }
 
+  // Méthode pour obtenir le nombre d'utilisateurs dans la base de données
   async nbUsers() {
     return this.db.collection('users').countDocuments();
   }
 
+  // Méthode pour obtenir le nombre de fichiers dans la base de données
   async nbFiles() {
     return this.db.collection('files').countDocuments();
   }
 }
 
+// Création d'une instance de DBClient
 const dbClient = new DBClient();
+// Exportation de l'instance pour être utilisée dans d'autres fichiers
 export default dbClient;
