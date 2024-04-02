4a61e0d31613e12878fa2c0fb73f6046ecffc05c
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-04-02 11:24:40 +0200

    update

--- utils/redis.js
+++ utils/redis.js
@@ -1,33 +1,44 @@
-import { createClient } from 'redis';
+const redis = require('redis');
+const { promisify } = require('util');
 
+// Définition de la classe RedisClient
 class RedisClient {
   constructor() {
-    this.client = createClient();
-    this.client.connect().catch((error) => {
-      console.log(`Redis client not connected to the server: ${error.message}`);
-    });
+    // Création d'un client Redis
+    this.client = redis.createClient();
 
-    this.client.on('error', (error) => {
-      console.log(`Redis client not connected to the server: ${error}`);
+    // Conversion des méthodes du client en versions asynchrones avec promisify
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+
+    // Gestion des erreurs du client Redis
+    this.client.on('error', (ERROR) => {
+      console.error(`error redis client: ${ERROR}`);
     });
   }
 
+  // Méthode pour vérifier si le client Redis est connecté
   isAlive() {
-    return this.client.isOpen;
+    return this.client.connected;
   }
 
+  // Méthode pour récupérer une valeur à partir d'une clé
   async get(key) {
-    return this.client.get(key);
+    return this.getAsync(key);
   }
 
+  // Méthode pour stocker une valeur avec une durée d'expiration
   async set(key, value, duration) {
-    await this.client.set(key, value, { EX: duration });
+    return this.setAsync(key, value, 'EX', duration);
   }
 
+  // Méthode pour supprimer une clé et sa valeur correspondante
   async del(key) {
-    await this.client.del(key);
+    return this.delAsync(key);
   }
 }
 
+// Création d'une instance de RedisClient
 const redisClient = new RedisClient();
-export default redisClient;
+module.exports = redisClient;
