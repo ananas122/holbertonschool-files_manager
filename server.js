19553bfdd47a973cd66e57033d3edd37578c2994
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-04-11 14:43:57 +0200

    Update

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
-const port = process.env.PORT || 5000;
-const routes = require('./routes');
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
